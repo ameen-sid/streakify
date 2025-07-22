81c4388d2382e94bdd3886b3417297afd3ff8074
Ameen Sid <ameensid7@outlook.com>
2025-07-22 20:06:36 +0530

    Add cloudinary configuration

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+const cloudinaryConnect = async () => {
+	try {
+
+		if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+			
+			throw new Error("Environment variables are not defined.");
+		}
+
+		cloudinary.config({
+			cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+			api_key: process.env.CLOUDINARY_API_KEY,
+			api_secret: process.env.CLOUDINARY_API_SECRET,
+		});
+		console.log("Connected to Cloudinary Successfully");
+	} catch(error: unknown) {
+		console.error("Error in connecting to cloudinary: ", error);
+	}
+};
+
+export default cloudinaryConnect;
