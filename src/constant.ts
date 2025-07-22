f0b73367cb2d93d29e522cce51f2d6324ff4facc
Ameen Sid <ameensid7@outlook.com>
2025-07-22 20:57:50 +0530

    Add some constants

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,20 @@
 // Datebase name
 const DB_NAME = "DisciplinePlanner";
 
+// Mail Type
+const MAIL_TYPES = {
+	welcome: "WELCOME",
+	otp: "OTP",
+	reset: "RESET",
+	daily_remainder: "DAILY_REMAINDER",
+	inactive_user_reengagement: "INACTIVE_USER_REENGAGEMENT",
+	streak_milestone: "STREAK_MILESTONE",
+	weekly_progress: "WEEKLY_PROGRESS",
+	delete: "DELETE",
+	recover: "RECOVER",
+};
+
 export {
 	DB_NAME,
+	MAIL_TYPES,
 };
