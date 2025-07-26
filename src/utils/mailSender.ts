e6f4410fe0fe26d9851e0a1df4634a221af63cc8
Ameen Sid <ameensid7@outlook.com>
2025-07-26 21:53:57 +0530

    Fix mailSender utility method

--- src/utils/mailSender.ts
+++ src/utils/mailSender.ts
@@ -13,7 +13,7 @@ const mailSender = async ({
 
 		const transporter = nodemailer.createTransport({
 			host: process.env.MAIL_HOST,
-			port: Number(process.env.MAIL_PORT),
+			// port: Number(process.env.MAIL_PORT),
 			// secure: false,
 			auth: {
 				user: process.env.MAIL_USER,
