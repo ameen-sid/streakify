6ead31f4eb0b415af9056531961651450310432d
Ameen Sid <ameensid7@outlook.com>
2025-08-01 21:10:01 +0530

    Again Fix asyncHandler utility for optinal parameters

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -3,18 +3,13 @@ import { HTTP_STATUS } from "@/constant";
 import { APIError } from "./APIError";
 import { APIResponse } from "./APIResponse";
 
-const asyncHandler = <
-	TParams extends Record<string, string> = Record<string, string>
->(
-	fn: (
-		req: NextRequest, 
-		context?: { params?: TParams }
-	) => Promise<NextResponse>
-): (req: NextRequest, context?: { params?: Record<string, string> }) => Promise<NextResponse> => {
-	return async (req: NextRequest, context?: { params?: Record<string, string> }): Promise<NextResponse> => {
+type AsyncFunction = (req: NextRequest, ...args: any[]) => Promise<NextResponse>;
+
+const asyncHandler = (fn: AsyncFunction) => {
+	return async (req: NextRequest, ...args: any[]): Promise<NextResponse> => {
 		try {
 
-			return await fn(req, { params: context?.params as TParams });
+			return await fn(req, ...args);
 		} catch (error) {
 
 			console.error("API Error Caught: ", error);
