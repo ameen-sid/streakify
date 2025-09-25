c69109a93ccbf44fae27de49a3452ccafbbb7a8b
Ameen Sid <ameensid7@outlook.com>
2025-09-26 00:15:00 +0530

    Fix database import-export

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,43 +4,43 @@ import { DB_NAME } from "@/constant";
 
 declare global {
 	var mongoose: {
-    	conn: Mongoose | null;
-    	promise: Promise<Mongoose> | null;
-  	};
+		conn: Mongoose | null;
+		promise: Promise<Mongoose> | null;
+	};
 }
 
 const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
-  throw new Error("MONGODB_URI environment variable is not defined.");
+	throw new Error("MONGODB_URI environment variable is not defined.");
 }
 
 let cached = global.mongoose;
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+	cached = global.mongoose = { conn: null, promise: null };
 }
 
 const connectDB = async () => {
-		
+
 	if (cached.conn) return cached.conn;
 
 	if (!cached.promise) {
-    	
+
 		cached.promise = mongoose.connect(`${MONGODB_URI}/${DB_NAME}`, {
-      		bufferCommands: false,
+			bufferCommands: false,
 			serverSelectionTimeoutMS: 10000,
-    	}).then((mongooseInstance) => {
-      		
+		}).then((mongooseInstance) => {
+
 			console.log("MongoDB connected");
-      		return mongooseInstance;
-    	}).catch((error) => {
-      		
+			return mongooseInstance;
+		}).catch((error) => {
+
 			console.error("MongoDB connection error:", error);
-      		throw error;
-    	});
-  	}
+			throw error;
+		});
+	}
 
 	cached.conn = await cached.promise;
-  	return cached.conn;
+	return cached.conn;
 }
 
 export default connectDB;
